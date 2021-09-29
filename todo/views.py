f5fd55a918711e6c6415b53d2289ea9f0dd1107e
renatalantos <renatalantos@gmail.com>
2021-09-29 16:27:23 +0000

    Add functionality to toggle and delete items

--- todo/views.py
+++ todo/views.py
@@ -35,9 +35,22 @@ def edit_item(request, item_id):
         if form.is_valid():
             form.save()
         return redirect('get_todo_list')
+    
     form = ItemForm(instance = item)
     context = {
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
     
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')    
