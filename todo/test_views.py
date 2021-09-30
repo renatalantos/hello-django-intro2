c5bb8795e649f034141b843f221bb084dfc9a20a
renatalantos <renatalantos@gmail.com>
2021-09-30 14:52:08 +0000

    Create tests for test_models.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -72,4 +72,13 @@ class TestViews(TestCase):
         updated_item = Item.objects.get(id = item.id)
         self.assertFalse(updated_item.done)
         # use assert false to check it's done status.
+
+    # Testing POST method in edit_item view
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name="Test Todo Item")
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id = item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')     
    
