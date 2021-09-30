55b2ab2658cb7f23b7ff2d4938c16e5af9b541ae
renatalantos <renatalantos@gmail.com>
2021-09-30 18:03:08 +0000

    Added secret key to to gitpod workspace and Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iji%5usmh2n-y^j8q0_jq$avjga+4iymext^o_)z9p2k84inr+')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
